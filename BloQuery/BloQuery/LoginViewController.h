5ab69b88a568c9cf3ddb6959f7046afdcace1a05
Eddy Chan <echan162@gmail.com>
2016-08-29 18:29:25 -0700

    Add a login view

---
+++ BloQuery/BloQuery/LoginViewController.h
@@ -0,0 +1,13 @@
+//
+//  LoginViewController.h
+//  BloQuery
+//
+//  Created by Eddy Chan on 8/25/16.
+//  Copyright © 2016 Eddy Chan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LoginViewController : UIViewController
+
+@end
